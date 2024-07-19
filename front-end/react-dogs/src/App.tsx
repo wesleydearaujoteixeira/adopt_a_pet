ab6ad1e15c9864240005b5a39728b6146b7af118
wesley <wesleyaraujoteixeira98@gmail.com>
2024-07-18 22:13:35 -0300

    sldksld

---
+++ front-end/react-dogs/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (  
+    <main> 
+      <h1>
+        I'ts running on React's Application!
+      </h1>
+  </main>
+
+  );
+}
+
+export default App;
